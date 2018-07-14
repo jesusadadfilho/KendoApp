fd32c970ed3f5dcaa4cdde22b982feacb8047338
jesusadadfilho <jesusadadfilho@gmail.com>
2018-07-14 12:46:18 -0300

    adicao do login

---
+++ app/src/main/java/com/example/jesus/kendotrannig/app/LoginActivity.java
@@ -0,0 +1,97 @@
+package com.example.jesus.kendotrannig.app;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.jesus.kendotrannig.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.net.Authenticator;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private SignInButton signInButton;
+    private GoogleApiClient mGoogleApiClient;
+    public SharedPreferences preferences;
+    private static final String TAG = "signInActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private static final int SIGIN_IN_CODE = 777;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        preferences = getSharedPreferences("reader.file", MODE_PRIVATE);
+        setupViews();
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN || requestCode == SIGIN_IN_CODE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectFalied: " + connectionResult);
+        Toast.makeText(this,"Falha ao logar",Toast.LENGTH_SHORT);
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "onConnectFalied: " + result.isSuccess());
+        if (result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("userName", account.getDisplayName());
+            editor.putString("userImageUrl", account.getPhotoUrl().toString());
+            editor.putString("userEmail", account.getEmail());
+            editor.putString("userId", account.getId());
+            editor.apply();
+            Toast.makeText(this, "Bem vindo : " + account.getEmail() +"!" ,Toast.LENGTH_SHORT).show();
+            //startActivity(new Intent(this, MainActivity.class));
+            //finish();
+
+
+        }
+    }
+
+    private void setupViews() {
+        signInButton = findViewById(R.id.singin_button);
+    }
+
+    public void singIn(View view) {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+        //Toast.makeText(this, "Clicando",Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/example/jesus/kendotrannig/app/MainActivity.java
+++ app/src/main/java/com/example/jesus/kendotrannig/app/MainActivity.java
@@ -1,19 +1,78 @@
 package com.example.jesus.kendotrannig.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.jesus.kendotrannig.R;
 import com.example.jesus.kendotrannig.model.Treino;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
+    private TextView userNameText, emailText;
+    private ImageView userImage;
+    public SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupViews();
+        setupDrawer();
+        preferences = getSharedPreferences("reader.file", MODE_PRIVATE);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViews() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        userNameText = navigationView.getHeaderView(0).findViewById(R.id.username_perfil);
+        emailText = navigationView.getHeaderView(0).findViewById(R.id.email_perfil);
+        userImage = navigationView.getHeaderView(0).findViewById(R.id.image_perfil);
+    }
+
+    private void setupDrawer() {
+        toggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(this);
+        /*if (!preferences.getString("userName"," ").equals(" ") &&
+                !preferences.getString("userImageUrl"," ").equals(" ") &&
+                !preferences.getString("userEmail"," ").equals(" "))
+        {
+            userNameText.setText(preferences.getString("userName","invalido"));
+            emailText.setText(preferences.getString("userEmail","invalido"));
+            Glide.with(this).load(preferences.getString("userImageUrl","https://firebasestorage.googleapis.com/v0/b/kendo-tranning.appspot.com/o/icon_not_found.png?alt=media&token=7ef72b7c-e316-4d8f-8fa0-5c0e119ec94a")).into(userImage);
+
+        }*/
     }
 
     public void irParaTreinos(View view) {
