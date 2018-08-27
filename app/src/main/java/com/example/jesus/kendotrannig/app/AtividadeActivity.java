2b755128bf339d3b1c166fb6d4d9f1cd48a92569
jesusadadfilho <jesusadadfilho@gmail.com>
2018-08-27 17:45:20 -0300

    implementação da reprodução de videos

--- app/src/main/java/com/example/jesus/kendotrannig/app/AtividadeActivity.java
+++ app/src/main/java/com/example/jesus/kendotrannig/app/AtividadeActivity.java
@@ -2,14 +2,46 @@ package com.example.jesus.kendotrannig.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.jesus.kendotrannig.R;
+import com.example.jesus.kendotrannig.dao.ConfiguracaoYoutube;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class AtividadeActivity extends AppCompatActivity {
+public class AtividadeActivity extends YouTubeBaseActivity {
 
+    YouTubePlayerView youTubePlayerView;
+    Button btnPlay;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade);
+        btnPlay = findViewById(R.id.button);
+        youTubePlayerView = findViewById(R.id.youtube_view);
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("j1FNnnmzLlU");
+
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                youTubePlayerView.initialize(ConfiguracaoYoutube.getApiKey(), onInitializedListener);
+            }
+        });
     }
 }
